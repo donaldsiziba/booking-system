369f7ca1ca1f4e663361132a25111b57f5a4e90f
donald <donald@awesomatic.co.za>
2021-06-22 20:29:40 +0200

    salon-ui - milestone 1

---
+++ salon-ui/src/App.js
@@ -0,0 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     <Navbar bg="dark">
+        <Navbar.Brand>Ar Salon & Day Spa</Navbar.Brand>
+      </Navbar>
+      <div className="container">
+      <div className="center">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ salon-ui/src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from "react-bootstrap/Navbar";
+
+import './NavBar.css';
+
+const navBar = () => {
+    return (
+        <Navbar bg="dark">
+        <Navbar.Brand>Ar Salon & Day Spa</Navbar.Brand>
+      </Navbar>
+    );
+};
+
+export default navBar;
